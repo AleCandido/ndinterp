4b54a2c2ed1e74d1e86be7c926766bfe7fc4cfea
juacrumar <juacrumar@lairen.eu>
2023-07-04 17:25:41 +0200

    added benchmark

--- ndinterp_capi/example_lhapdf/lhacheck.cpp
+++ ndinterp_capi/example_lhapdf/lhacheck.cpp
@@ -1,15 +1,19 @@
 #include <cstdlib>
 #include <iostream>
-#include "LHAPDF/LHAPDF.h"
 #include <vector>
 #include <string>
 #include <sstream>
+#include <chrono>
+#include "LHAPDF/LHAPDF.h"
 
+
+// For the time being the header is manual
 typedef struct cubic1d cubic1d;
 extern "C" {
     cubic1d* create_cubic_interpolator1d(const double*, const double*, const size_t);
     double interpolate_cubic_1d(cubic1d*, const double);
 }
+
 
 std::vector<double> string_to_vector(const std::string& stri_raw) {
     // Take the arrays from the LHAPDF file and make them into arrays
@@ -43,20 +47,52 @@ int main() {
 
     cubic1d* my_grid = create_cubic_interpolator1d(q2s.data(), als.data(), q2s.size());
 
-    double qmin = 3.0;
-    double qmax = 100.0;
+    const int n = 1000000;
+    const double qmin = 3.0;
+    const double qmax = 100.0;
 
-    for (int i = 0; i < 1000; i++) {
+    std::vector<double> qvals;
+    std::vector<double> ndinterp_results;
+    std::vector<double> lhapdf_results;
+    bool ifail = false;
+
+    for (int i = 0; i < n; i++) {
         double rn = (double) rand() / RAND_MAX;
         double q = qmin + rn*(qmax-qmin);
-        double q2 = q*q;
+        qvals.push_back(q);
+    }
 
+    std::cout << "Benchmarking the timing!" << std::endl;
+    std::chrono::steady_clock::time_point st, et;
+
+    st = std::chrono::steady_clock::now();
+    for (double q: qvals) {
+        double lh_res = pdf->alphasQ2(q*q);
+        lhapdf_results.push_back(lh_res);
+    }
+    et = std::chrono::steady_clock::now();
+    std::chrono::duration<double> lhapdf_time = std::chrono::duration_cast< std::chrono::duration<double> >(et - st);
+    std::cout << "while LHAPDF took " << lhapdf_time.count() << " seconds" << std::endl;
+
+    st = std::chrono::steady_clock::now();
+    for (double q: qvals) {
         double my_res = interpolate_cubic_1d(my_grid, q);
-        double lh_res = pdf->alphasQ2(q2);
+        ndinterp_results.push_back(my_res);
+    }
+    et = std::chrono::steady_clock::now();
+    std::chrono::duration<double> ndinterp_time = std::chrono::duration_cast< std::chrono::duration<double> >(et - st);
+    std::cout << "ndinterp took " << ndinterp_time.count() << " seconds" << std::endl;
 
-        if ( (my_res-lh_res)/my_res > 1e-4 ) {
-            std::cout << "Error for q=" << q << std::endl;
+    std::cout << "Checking whether the results agree" << std::endl;
+    // Now compare the results
+    for (int i = 0; i < qvals.size(); i++) {
+        if( (ndinterp_results[i]-lhapdf_results[i])/ndinterp_results[i] > 1e-4 ) {
+            std::cout << "Error for q=" << qvals[i] << std::endl;
+            ifail = true;
         }
+    }
+    if (!ifail) {
+        std::cout << "All tested points agreed with LHAPDF ✅" << std::endl;
     }
 
     return 0;
